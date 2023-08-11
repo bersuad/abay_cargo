e044543c9367730efb9171454b5a7b7fb0bdc43b
bersuad <bersuadane@gmail.com>
2023-08-11 22:18:27 +0300

    header styling

--- src/pages/transporterNotification.js
+++ src/pages/transporterNotification.js
@@ -6,8 +6,8 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 const NotificationBar = () => {
   return (
     <View style={{flexDirection: 'row'}}>
-      <Ionicons name="md-notifications" size={20} color="#19788e" style={{marginRight: '25%'}}/>
-      <View style={{width: 20, height: 20, backgroundColor: 'rgba(25, 120, 142, 0.3)', alignItems: "center", justifyContent: "center", borderRadius: '100%', position: 'absolute', marginLeft: '8%', marginTop: '-5%'}}>
+      <Ionicons name="md-notifications" size={20} color="#19788e" style={{marginRight: 25}}/>
+      <View style={{width: 25, height: 25, backgroundColor: 'rgba(25, 120, 142, 0.3)', alignItems: "center", justifyContent: "center", borderRadius: 100, position: 'absolute', marginLeft: 10, marginTop: -5}}>
         <Text>10</Text>
       </View>
     </View>
